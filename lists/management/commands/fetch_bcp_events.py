b4560fdc3d9b3156b55f6abdb117609cadc2e39d
Tymoteusz Paul <paul@hyll.com>
2023-11-17 10:26:38 +0000

    feat: scrape events themselves from bcp

---
+++ lists/management/commands/fetch_bcp_events.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from lists.models import Event, BCP
+import requests
+
+
+class Command(BaseCommand):
+    help = "Fetch events from BCP"
+
+    def handle(self, *args, **options):
+        month = 1
+        year = 2023
+        limit = 100
+        headers = settings.BCP_HEADERS
+        while month <= 12:
+            url = f"https://prod-api.bestcoastpairings.com/events?limit={limit}&startDate={year}-{month:02d}-01T00%3A00%3A00Z&endDate={year}-{month:02d}-31T00%3A00%3A00Z&sortKey=eventDate&sortAscending=true&gameType=4"
+            response = requests.get(url, headers=headers)
+            if response.status_code != 200:
+                raise CommandError(
+                    f"Failed to fetch data for {year}-{month:02d}, code: {response.status_code} body response: {response.text}"
+                )
+            data = response.json()
+            for event in data["data"]:
+                event_dict = {
+                    "source": BCP,
+                    "source_id": event["id"],
+                    "source_json": event,
+                    "name": event["name"],
+                    "start_date": event["eventDate"],
+                    "end_date": event["eventEndDate"],
+                }
+                if "numTickets" in event:
+                    event_dict["players_count"] = event["numTickets"]
+                Event.objects.update_or_create(
+                    source=BCP, source_id=event["id"], defaults=event_dict
+                )
+            self.stdout.write(
+                self.style.SUCCESS(f"Successfully fetched data for {year}-{month:02d}")
+            )
+            month += 1
--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
 # Create your models here.
+
+BCP = 0
+TSN = 1
+
+event_sources = (
+    (BCP, "BCP"),
+    (TSN, "TSN"),
+)
+
+
+class Location(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
+
+class Event(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    source = models.IntegerField(choices=event_sources)
+    source_id = models.CharField(max_length=100)
+    source_json = models.JSONField()
+    name = models.CharField(max_length=100)
+    start_date = models.DateTimeField(null=True)
+    end_date = models.DateTimeField(null=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True)
+    photo_url = models.URLField(null=True)
+    players_count = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["start_date"]
+        indexes = [
+            models.Index(fields=["name", "start_date"]),
+            models.Index(fields=["start_date"]),
+            models.Index(fields=["end_date"]),
+            models.Index(fields=["source", "source_id"]),
+        ]
+
+
+class Player(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    source = models.IntegerField(choices=event_sources)
+    source_id = models.CharField(max_length=100)
+    source_json = models.JSONField()
+
+
+class Participant(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
