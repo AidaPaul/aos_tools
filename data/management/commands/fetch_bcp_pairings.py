359bbc7fd88e5e3ee46908adf9fc10c2ea3cfee0
Tymoteusz Paul <paul@hyll.com>
2023-12-06 09:07:00 +0000

    feat: migrate pairing logic to a task

--- data/management/commands/fetch_bcp_pairings.py
+++ data/management/commands/fetch_bcp_pairings.py
@@ -3,135 +3,37 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Q, Count
 
 from data.models import *
+from data.tasks import fetch_pairings_for_event
 import requests
 
 
 class Command(BaseCommand):
-    help = "Fetch events from BCP"
+    help = "Fetch pairings for events from BCP"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--celery",
+            action="store_true",
+            dest="celery",
+            default=False,
+            help="Use celery to fetch lists",
+        )
 
     def handle(self, *args, **options):
         headers = settings.BCP_HEADERS
-        for event in (
-            Event.objects.filter(source=BCP)
-            .annotate(pairings_count=Count("pairings"))
-            .exclude(pairings_count__gt=2)
-        ):
-            round = 0
-            max_rounds = event.rounds
-            if max_rounds is None:
-                continue
-            while round <= max_rounds:
-                round += 1
-                url = f"https://prod-api.bestcoastpairings.com/pairings?limit=100&eventId={event.source_id}&round={round}&pairingType=Pairing&expand%5B%5D=player1&expand%5B%5D=player2&expand%5B%5D=player1Game&expand%5B%5D=player2Game"
-                response = requests.get(url, headers=headers)
-                if response.status_code != 200:
-                    self.stderr.write(
-                        self.style.ERROR(
-                            f"Failed to fetch data for {event.name}, code: {response.status_code} body response: {response.text}"
-                        )
-                    )
-                    continue
-                data = response.json()
-                player1 = None
-                player2 = None
-                for pairing in data["data"]:
-                    if len(pairing["player1"]) == 0 or len(pairing["player2"]) == 0:
-                        self.stdout.write(
-                            self.style.ERROR(
-                                f"Failed to fetch data for {event.name}, no player1 or player2"
-                            )
-                        )
-                        continue
-                    pairing_dict = {
-                        "source_id": pairing["id"],
-                        "source_json": pairing,
-                        "event": event,
-                        "round": round,
-                    }
-                    for player in ["player1", "player2"]:
-                        player_dict = {
-                            "source": BCP,
-                            "source_json": pairing[player],
-                        }
-                        if "id" in pairing[player]:
-                            player_dict["source_id"] = pairing[player]["id"]
-                        else:
-                            self.stderr.write(
-                                self.style.ERROR(
-                                    f"Failed to fetch data for {event.name}, no id in {player}"
-                                )
-                            )
-                            continue
-                        player_instance = Player.objects.update_or_create(
-                            source=BCP,
-                            source_id=pairing[player]["id"],
-                            defaults=player_dict,
-                        )[0]
-                        participant_dict = {
-                            "event": event,
-                            "player": player_instance,
-                            "source_json": pairing[player],
-                        }
-                        if "armyListObjectId" in pairing[player]:
-                            participant_dict["army_source_id"] = pairing[player][
-                                "armyListObjectId"
-                            ]
-                        if "armyId" in pairing[player]:
-                            participant_dict["army_id"] = pairing[player]["armyId"]
-                        participant_instance = Participant.objects.update_or_create(
-                            event=event,
-                            player=player_instance,
-                            defaults=participant_dict,
-                        )[0]
-                        if player == "player1":
-                            player1 = participant_instance
-                        else:
-                            player2 = participant_instance
-                    pairing_dict["player1"] = player1
-                    pairing_dict["player2"] = player2
-                    if "gamePoints" in pairing["player1Game"]:
-                        pairing_dict["player1_score"] = pairing["player1Game"][
-                            "gamePoints"
-                        ]
-                    if "gamePoints" in pairing["player2Game"]:
-                        pairing_dict["player2_score"] = pairing["player2Game"][
-                            "gamePoints"
-                        ]
-                    if "gameResult" in pairing["player1Game"]:
-                        pairing_dict["player1_result"] = pairing["player1Game"][
-                            "gameResult"
-                        ]
-                    if "gameResult" in pairing["player2Game"]:
-                        pairing_dict["player2_result"] = pairing["player2Game"][
-                            "gameResult"
-                        ]
-                    if "armyListObjectId" in pairing["player1"]:
-                        pairing_dict["player1_list"] = List.objects.update_or_create(
-                            source_id=pairing["player1"]["armyListObjectId"],
-                            defaults={
-                                "source_json": pairing["player1"],
-                                "participant": player1,
-                            },
-                        )[0]
-                    if "armyListObjectId" in pairing["player2"]:
-                        pairing_dict["player2_list"] = List.objects.update_or_create(
-                            source_id=pairing["player2"]["armyListObjectId"],
-                            defaults={
-                                "source_json": pairing["player2"],
-                                "participant": player2,
-                            },
-                        )[0]
-                    try:
-                        Pairing.objects.update_or_create(
-                            source_id=pairing["pairingId"], defaults=pairing_dict
-                        )
-                    except Exception as e:
-                        self.stderr.write(
-                            self.style.ERROR(
-                                f"Failed to save pairing for {event.name}, error: {e}"
-                            )
-                        )
-                        continue
-                self.stdout.write(
-                    self.style.SUCCESS(f"Successfully fetched data for {event.name}")
-                )
+        events = (
+            Event.objects.filter(source=BCP).annotate(pairings_count=Count("pairings"))
+            # .exclude(pairings_count__gt=2)
+        )
+        self.stdout.write(f"Fetching data for {events.count()} events")
+        tasks = []
+        for event in events:
+            if options["celery"]:
+                tasks.append(fetch_pairings_for_event.delay(event.id))
+            else:
+                fetch_pairings_for_event(event.id)
+
+        if options["celery"]:
+            self.stdout.write(f"Started {len(tasks)} tasks")
+        else:
+            self.stdout.write(f"Finished fetching pairings")
--- data/tasks.py
+++ data/tasks.py
@@ -8,6 +8,13 @@ headers = settings.BCP_HEADERS
 
 from celery.contrib import rdb
 
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q, Count
+
+from data.models import *
+import requests
+
 
 @shared_task
 def fetch_list(list_id: int):
@@ -24,3 +31,101 @@ def fetch_list(list_id: int):
         current_list.raw_list = data["armyListText"]
     current_list.save()
     return True
+
+
+@shared_task()
+def fetch_pairings_for_event(event_id: int):
+    event = Event.objects.get(id=event_id)
+    round = 0
+    max_rounds = event.rounds
+    if max_rounds is None:
+        return
+    while round <= max_rounds:
+        round += 1
+        url = f"https://prod-api.bestcoastpairings.com/pairings?limit=100&eventId={event.source_id}&round={round}&pairingType=Pairing&expand%5B%5D=player1&expand%5B%5D=player2&expand%5B%5D=player1Game&expand%5B%5D=player2Game"
+        response = requests.get(url, headers=headers)
+        if response.status_code != 200:
+            raise Exception(
+                f"Failed to fetch data for {event.name}, code: {response.status_code} body response: {response.text}"
+            )
+        data = response.json()
+        player1 = None
+        player2 = None
+        for pairing in data["data"]:
+            if len(pairing["player1"]) == 0 or len(pairing["player2"]) == 0:
+                continue
+            pairing_dict = {
+                "source_id": pairing["id"],
+                "source_json": pairing,
+                "event": event,
+                "round": round,
+            }
+            for player in ["player1", "player2"]:
+                player_dict = {
+                    "source": BCP,
+                    "source_json": pairing[player],
+                }
+                if "id" in pairing[player]:
+                    player_dict["source_id"] = pairing[player]["id"]
+                else:
+                    raise Exception(
+                        f"Failed to fetch data for {event.name}, no player id in pairing"
+                    )
+                player_instance = Player.objects.update_or_create(
+                    source=BCP,
+                    source_id=pairing[player]["id"],
+                    defaults=player_dict,
+                )[0]
+                participant_dict = {
+                    "event": event,
+                    "player": player_instance,
+                    "source_json": pairing[player],
+                }
+                if "armyListObjectId" in pairing[player]:
+                    participant_dict["army_source_id"] = pairing[player][
+                        "armyListObjectId"
+                    ]
+                if "armyId" in pairing[player]:
+                    participant_dict["army_id"] = pairing[player]["armyId"]
+                participant_instance = Participant.objects.update_or_create(
+                    event=event,
+                    player=player_instance,
+                    defaults=participant_dict,
+                )[0]
+                if player == "player1":
+                    player1 = participant_instance
+                else:
+                    player2 = participant_instance
+            pairing_dict["player1"] = player1
+            pairing_dict["player2"] = player2
+            if "gamePoints" in pairing["player1Game"]:
+                pairing_dict["player1_score"] = pairing["player1Game"]["gamePoints"]
+            if "gamePoints" in pairing["player2Game"]:
+                pairing_dict["player2_score"] = pairing["player2Game"]["gamePoints"]
+            if "gameResult" in pairing["player1Game"]:
+                pairing_dict["player1_result"] = pairing["player1Game"]["gameResult"]
+            if "gameResult" in pairing["player2Game"]:
+                pairing_dict["player2_result"] = pairing["player2Game"]["gameResult"]
+            if "armyListObjectId" in pairing["player1"]:
+                pairing_dict["player1_list"] = List.objects.update_or_create(
+                    source_id=pairing["player1"]["armyListObjectId"],
+                    defaults={
+                        "source_json": pairing["player1"],
+                        "participant": player1,
+                    },
+                )[0]
+            if "armyListObjectId" in pairing["player2"]:
+                pairing_dict["player2_list"] = List.objects.update_or_create(
+                    source_id=pairing["player2"]["armyListObjectId"],
+                    defaults={
+                        "source_json": pairing["player2"],
+                        "participant": player2,
+                    },
+                )[0]
+            try:
+                Pairing.objects.update_or_create(
+                    source_id=pairing["id"], defaults=pairing_dict
+                )
+            except Exception as e:
+                continue
+        return True
