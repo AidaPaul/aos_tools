9f9e93b6ce2d2b6711c9a92b7a940c81b5bd600c
Aida Paul <puciek@gmail.com>
2023-12-10 13:50:37 +0000

    feat: pick a game, any game for stats

--- data/urls.py
+++ data/urls.py
@@ -4,5 +4,9 @@ from data.views import raw_list, export_pairings_as_csv
 
 urlpatterns = [
     path("lists/<int:list_id>/raw", raw_list, name="raw_list"),
-    path("woestats/", export_pairings_as_csv, name="export_pairings_as_csv"),
+    path(
+        "woestats/<int:game_type>",
+        export_pairings_as_csv,
+        name="export_pairings_as_csv",
+    ),
 ]
--- data/views.py
+++ data/views.py
@@ -20,9 +20,11 @@ def raw_list(request, list_id):
     return HttpResponse(list.raw_list.replace("\n", "<br>"))
 
 
-def export_pairings_as_csv(request):
+def export_pairings_as_csv(request, game_type: int = AOS):
     pairings = Pairing.objects.filter(
-        event__start_date__range=["2023-07-15", "2023-12-31"], event__rounds__in=[3, 5]
+        Q(event__start_date__range=["2023-07-15", "2023-12-31"])
+        & Q(event__rounds__in=[3, 5])
+        & Q(event__game_type=game_type)
     ).order_by("event__name", "-event__start_date", "round", "id")
 
     response = HttpResponse(content_type="text/csv")
